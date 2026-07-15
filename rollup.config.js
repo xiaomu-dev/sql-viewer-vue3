e2f3d71e1660ad698b5751f5461b71297a1f4fe1
xiaomu_dev <xiaomu_dev@163.com>
2026-07-15 16:46:52 +0800

    什么辣鸡豆包

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
-import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
 import vue from 'rollup-plugin-vue';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import scss from 'rollup-plugin-scss';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default {
 	input: resolve(__dirname, 'src/index.js'),
